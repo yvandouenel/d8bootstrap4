cb92bdebdf38a85d3eb54dad2e7029facb47dcef
lorenzo19981998 <loz19981998@gmail.com>
2019-10-22 09:30:41 +0200

    drupal 8 installed with bootstrap 4.

---
+++ web/themes/custom/memo/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.memo = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
